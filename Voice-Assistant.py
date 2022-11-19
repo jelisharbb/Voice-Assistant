e05c0a1c93c55692880333e9a0e25647825aa16b
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-11-19 11:14:25 +0800

    Imported and used a module to detect the user's voice and convert it to text.

---
+++ Voice-Assistant.py
@@ -0,0 +1,23 @@
+# Say "Hey, Jarvis"
+# Jarvis will listen, then you will talk
+# Jarvis will respond
+# reference: https://www.youtube.com/watch?v=AWvsXxDtEkU
+
+
+# importing modules
+import speech_recognition as sr
+
+listener = sr.Recognizer()
+
+# try and except statement to catch the error prevent the program from crashing
+try:
+    with sr.Microphone() as source:
+        print()
+        print("Hi! I'm listening...")
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        print(f"You: {command.capitalize()}.")
+        print()
+
+except:
+    pass
